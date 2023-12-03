61f70f9b8531ba78ff68f83d02507bd28ab7577a
Dan Souza <darleyble3@gmail.com>
2023-12-03 19:29:22 -0300

    folder rename and add menu

--- prova-grupo/DataFrutaModificado.py
+++ prova-grupo/DataFrutaModificado.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-from datetime import date
+from datetime import datetime
+
 
 class Data:
-    
     def __init__(self, dia=1, mes=1, ano=2000):
         if dia < 1 or dia > 31:
             raise ValueError("Dia inválido")
@@ -17,7 +17,7 @@ class Data:
     @property
     def dia(self):
         return self.__dia
-    
+
     @dia.setter
     def dia(self, dia):
         if dia < 1 or dia > 31:
@@ -27,7 +27,7 @@ class Data:
     @property
     def mes(self):
         return self.__mes
-    
+
     @mes.setter
     def mes(self, mes):
         if mes < 1 or mes > 12:
@@ -37,21 +37,23 @@ class Data:
     @property
     def ano(self):
         return self.__ano
-    
+
     @ano.setter
     def ano(self, ano):
         if ano < 2000 or ano > 2100:
             raise ValueError("Ano inválido")
         self.__ano = ano
-    
+
     def __str__(self):
         return "{}/{}/{}".format(self.__dia, self.__mes, self.__ano)
 
     def __eq__(self, outraData):
-        return  self.__dia == outraData.__dia and \
-                self.__mes == outraData.__mes and \
-                self.__ano == outraData.__ano
-    
+        return (
+            self.__dia == outraData.__dia
+            and self.__mes == outraData.__mes
+            and self.__ano == outraData.__ano
+        )
+
     def __lt__(self, outraData):
         if self.__ano < outraData.__ano:
             return True
@@ -62,7 +64,7 @@ class Data:
                 if self.__dia < outraData.__dia:
                     return True
         return False
-    
+
     def __gt__(self, outraData):
         if self.__ano > outraData.__ano:
             return True
@@ -74,8 +76,8 @@ class Data:
                     return True
         return False
 
-class AnaliseDados(ABC): 
 
+class AnaliseDados(ABC):
     @abstractmethod
     def __init__(self, tipoDeDados):
         self.__tipoDeDados = tipoDeDados
@@ -87,7 +89,7 @@ class AnaliseDados(ABC):
     @abstractmethod
     def mostraMediana(self):
         pass
-    
+
     @abstractmethod
     def mostraMenor(self):
         pass
@@ -96,11 +98,11 @@ class AnaliseDados(ABC):
     def mostraMaior(self):
         pass
 
+
 class ListaNomes(AnaliseDados):
-    
     def __init__(self):
         super().__init__(str)
-        self.__lista = []        
+        self.__lista = []
 
     def entradaDeDados(self):
         n = int(input("Quantos nomes deseja adicionar? "))
@@ -112,11 +114,11 @@ class ListaNomes(AnaliseDados):
         sorted_list = sorted(self.__lista)
         n = len(sorted_list)
         if n % 2 == 0:
-            median1 = sorted_list[n//2 - 1]
-            median2 = sorted_list[n//2]
+            median1 = sorted_list[n // 2 - 1]
+            median2 = sorted_list[n // 2]
             print(f"Mediana: {median1}")
         else:
-            median = sorted_list[n//2]
+            median = sorted_list[n // 2]
             print(f"Mediana: {median}")
 
     def mostraMenor(self):
@@ -128,18 +130,20 @@ class ListaNomes(AnaliseDados):
     def __str__(self):
         return ", ".join(self.__lista)
 
+
 class ListaDatas(AnaliseDados):
-        
     def __init__(self):
         super().__init__(Data)
-        self.__lista = []        
-    
+        self.__lista = []
+
     def entradaDeDados(self):
         n = int(input("Quantas datas deseja adicionar? "))
         for _ in range(n):
-            dia = int(input("Digite o dia: "))
-            mes = int(input("Digite o mês: "))
-            ano = int(input("Digite o ano: "))
+            print("Digite a data no formato dia/mês/ano: ")
+            data_input = input().split("/")
+            dia = int(data_input[0])
+            mes = int(data_input[1])
+            ano = int(data_input[2])
             data = Data(dia, mes, ano)
             self.__lista.append(data)
 
@@ -147,27 +151,29 @@ class ListaDatas(AnaliseDados):
         sorted_list = sorted(self.__lista, key=lambda x: (x.ano, x.mes, x.dia))
         n = len(sorted_list)
         if n % 2 == 0:
-            median1 = sorted_list[n//2 - 1]
-            median2 = sorted_list[n//2]
+            median1 = sorted_list[n // 2 - 1]
+            median2 = sorted_list[n // 2]
             print(f"Mediana: {median1}")
         else:
-            median = sorted_list[n//2]
+            median = sorted_list[n // 2]
             print(f"Mediana: {median}")
 
     def mostraMenor(self):
-        print(f"Menor: {min(self.__lista)}")
+        menor_data = min(self.__lista, key=lambda x: (x.ano, x.mes, x.dia))
+        print(f"Menor: {menor_data}")
 
     def mostraMaior(self):
-        print(f"Maior: {max(self.__lista)}")
+        maior_data = max(self.__lista, key=lambda x: (x.ano, x.mes, x.dia))
+        print(f"Maior: {maior_data}")
 
     def __str__(self):
-        return "\n".join(map(str, self.__lista))
+        return "\n".join(str(data) for data in self.__lista)
+
 
 class ListaSalarios(AnaliseDados):
-
     def __init__(self):
         super().__init__(float)
-        self.__lista = []        
+        self.__lista = []
 
     def entradaDeDados(self):
         n = int(input("Quantos salários deseja adicionar? "))
@@ -179,12 +185,12 @@ class ListaSalarios(AnaliseDados):
         sorted_list = sorted(self.__lista)
         n = len(sorted_list)
         if n % 2 == 0:
-            median1 = sorted_list[n//2 - 1]
-            median2 = sorted_list[n//2]
+            median1 = sorted_list[n // 2 - 1]
+            median2 = sorted_list[n // 2]
             median = (median1 + median2) / 2
             print(f"Mediana: {median:.2f}")
         else:
-            median = sorted_list[n//2]
+            median = sorted_list[n // 2]
             print(f"Mediana: {median:.2f}")
 
     def mostraMenor(self):
@@ -196,12 +202,12 @@ class ListaSalarios(AnaliseDados):
     def __str__(self):
         return ", ".join(map(str, self.__lista))
 
+
 class ListaIdades(AnaliseDados):
-    
     def __init__(self):
         super().__init__(int)
-        self.__lista = []        
-    
+        self.__lista = []
+
     def entradaDeDados(self):
         n = int(input("Quantas idades deseja adicionar? "))
         for _ in range(n):
@@ -212,12 +218,12 @@ class ListaIdades(AnaliseDados):
         sorted_list = sorted(self.__lista)
         n = len(sorted_list)
         if n % 2 == 0:
-            median1 = sorted_list[n//2 - 1]
-            median2 = sorted_list[n//2]
+            median1 = sorted_list[n // 2 - 1]
+            median2 = sorted_list[n // 2]
             median = (median1 + median2) / 2
             print(f"Mediana: {median:.2f}")
         else:
-            median = sorted_list[n//2]
+            median = sorted_list[n // 2]
             print(f"Mediana: {median:.2f}")
 
     def mostraMenor(self):
@@ -229,22 +235,53 @@ class ListaIdades(AnaliseDados):
     def __str__(self):
         return ", ".join(map(str, self.__lista))
 
+
 def main():
     nomes = ListaNomes()
     datas = ListaDatas()
     salarios = ListaSalarios()
     idades = ListaIdades()
-
     listaListas = [nomes, datas, salarios, idades]
 
-    for lista in listaListas:
-        lista.entradaDeDados()
-        lista.mostraMediana()
-        lista.mostraMenor()
-        lista.mostraMaior()
-        print("_______________________________________")
+    while True:
+        print("\n------ Menu ------")
+        print("1. Nomes\n2. Datas\n3. Salarios\n4. Idades\n5. Sair")
 
-    print("*** Fim do teste ***")
+        opcao = input("Selecione a opção: ")
+
+        print("\n")
+
+        match (opcao):
+            case "1":
+                listaListas[0].entradaDeDados()
+                listaListas[0].mostraMediana()
+                listaListas[0].mostraMenor()
+                listaListas[0].mostraMaior()
+                print("------------------------------------\n\n")
+            case "2":
+                listaListas[1].entradaDeDados()
+                listaListas[1].mostraMediana()
+                listaListas[1].mostraMenor()
+                listaListas[1].mostraMaior()
+                print("------------------------------------\n")
+            case "3":
+                listaListas[2].entradaDeDados()
+                listaListas[2].mostraMediana()
+                listaListas[2].mostraMenor()
+                listaListas[2].mostraMaior()
+                print("------------------------------------\n")
+            case "4":
+                listaListas[3].entradaDeDados()
+                listaListas[3].mostraMediana()
+                listaListas[3].mostraMenor()
+                listaListas[3].mostraMaior()
+                print("------------------------------------\n")
+            case "5":
+                print("Saindo...")
+                break
+            case _:
+                print("Opção inválida. Por favor, selecione uma opção válida.")
+
 
 if __name__ == "__main__":
     main()
